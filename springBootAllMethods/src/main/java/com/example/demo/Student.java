22bf0c96adb7b12accfed6202d800db8f64bd803
vishal rai <vishalrai10797@gmail.com>
2025-01-10 13:21:33 +0530

    Spring boot

--- springBootAllMethods/src/main/java/com/example/demo/Student.java
+++ springBootAllMethods/src/main/java/com/example/demo/Student.java
@@ -1,6 +1,46 @@
 package com.example.demo;
 
 public class Student {
+	 Long id;
+	    String name;
+	    int age;
+	    String standard;
+		public Long getId() {
+			return id;
+		}
+		public void setId(Long id) {
+			this.id = id;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public int getAge() {
+			return age;
+		}
+		public void setAge(int age) {
+			this.age = age;
+		}
+		public String getStandard() {
+			return standard;
+		}
+		public void setStandard(String standard) {
+			this.standard = standard;
+		}
+		public Student(Long id, String name, int age, String standard) {
+			super();
+			this.id = id;
+			this.name = name;
+			this.age = age;
+			this.standard = standard;
+		}
+		@Override
+		public String toString() {
+			return "Student [id=" + id + ", name=" + name + ", age=" + age + ", standard=" + standard + "]";
+		}
+
 	
 
 }
